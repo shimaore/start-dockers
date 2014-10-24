d700f5d7c37823e0cb877dfb99c0d9bab9119786
Stephane Alnet <stephane@shimaore.net>
2014-10-24 16:16:08 +0200

    Corrected install.

--- install.sh
+++ install.sh
@@ -6,9 +6,12 @@ adduser docker docker
 # Install the startup script.
 cat > ~docker/start.sh <<'SCRIPT'
 #!/bin/bash
-for vm in ~/start/*; do
+export DOCKER_BASE=~docker/start/
+for vm in "$DOCKER_BASE"/*; do
   (cd "$vm" && ./start.sh)
 done
 SCRIPT
 chmod +x ~docker/start.sh
+mkdir -p ~docker/start/
+chown docker.docker ~docker/start/
 echo @reboot ~docker/start.sh | crontab -u docker -
