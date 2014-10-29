cba8e94a33eca1e76599cb773445884ed2514b4e
Stephane Alnet <stephane@shimaore.net>
2014-10-29 09:25:28 +0100

    Added a stop script for ops & tests.

--- install.sh
+++ install.sh
@@ -13,6 +13,14 @@ for vm in "$DOCKER_BASE"/*; do
 done
 SCRIPT
 chmod +x ~docker/start.sh
+cat > ~docker/stop.sh <<'SCRIPT'
+#!/bin/bash
+export DOCKER_BASE=~docker/start/
+for vm in "$DOCKER_BASE"/*; do
+  (cd "$vm" && ./stop.sh)
+done
+SCRIPT
+chmod +x ~docker/stop.sh
 mkdir -p ~docker/start/
 chown docker.docker ~docker/start/
 echo @reboot ~docker/start.sh | crontab -u docker -
