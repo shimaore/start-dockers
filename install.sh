d2459a655d796e9b9421c562774a830c8dcf797b
Stephane Alnet <stephane@shimaore.net>
2014-10-29 09:57:30 +0100

    Export DOCKER_NAME

--- install.sh
+++ install.sh
@@ -8,16 +8,18 @@ getent passwd docker >/dev/null || /usr/sbin/useradd -m -g docker docker
 cat > ~docker/start.sh <<'SCRIPT'
 #!/bin/bash
 export DOCKER_BASE=~docker/start/
-for vm in "$DOCKER_BASE"/[0-9]*; do
-  (cd "$vm" && ./start.sh)
+cd "${DOCKER_BASE}" && for DOCKER_NAME in [0-9]*; do
+  export DOCKER_NAME
+  (cd "${DOCKER_NAME}" && ./start.sh)
 done
 SCRIPT
 chmod +x ~docker/start.sh
 cat > ~docker/stop.sh <<'SCRIPT'
 #!/bin/bash
 export DOCKER_BASE=~docker/start/
-for vm in "$DOCKER_BASE"/[0-9]*; do
-  (cd "$vm" && ./stop.sh)
+cd "${DOCKER_BASE}" && for DOCKER_NAME in [0-9]*; do
+  export DOCKER_NAME
+  (cd "${DOCKER_NAME}" && ./stop.sh)
 done
 SCRIPT
 chmod +x ~docker/stop.sh
