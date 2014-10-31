912c2fb34a398689bd35c7f38049ba0db5a3f69a
Stephane Alnet <stephane@shimaore.net>
2014-10-31 15:57:39 +0100

    Add log at startup.

--- install.sh
+++ install.sh
@@ -7,20 +7,24 @@ getent passwd docker >/dev/null || /usr/sbin/useradd -m -g docker --uid 9999 doc
 # Install the startup script.
 cat > ~docker/start.sh <<'SCRIPT'
 #!/bin/bash
+echo '**** start ****' >> ~docker/startup.log
+date >> ~docker/startup.log
 export DOCKER_BASE=~docker/start/
 cd "${DOCKER_BASE}" && for DOCKER_NAME in [0-9]*; do
   export DOCKER_NAME
   (cd "${DOCKER_NAME}" && ./init start)
-done
+done >> ~docker/startup.log
 SCRIPT
 chmod +x ~docker/start.sh
 cat > ~docker/stop.sh <<'SCRIPT'
 #!/bin/bash
+echo '**** stop ****' >> ~docker/startup.log
+date >> ~docker/startup.log
 export DOCKER_BASE=~docker/start/
 cd "${DOCKER_BASE}" && for DOCKER_NAME in [0-9]*; do
   export DOCKER_NAME
   (cd "${DOCKER_NAME}" && ./init stop)
-done
+done >> ~docker/startup.log
 SCRIPT
 chmod +x ~docker/stop.sh
 mkdir -p ~docker/start/
